ffe73d3b898a39df982d82e98eea30bd8bd7aa69
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-10-01 14:02:50 +0200

    first commit

---
+++ revision.js
@@ -0,0 +1,12 @@
+// 01 - Alphabet
+
+//- Créer une fonction `sortLetters` qui reçoit une 
+//string en paramètre
+//- La fonction retourne une string avec les mêmes 
+//lettres, dans l'ordre alphabétique
+// - Appelez votre fonction avec "konexio" comme 
+//argument et vérifiez que vous obtenez "eiknoox"
+
+function sortLetters("h") {
+    
+}
